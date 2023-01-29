0d12147aab7df127bea930b7510ea09aff6574f0
tfferdous <ferdous.themefisher@gmail.com>
2023-01-29 18:14:55 +0600

    fixed border

--- layouts/Contact.js
+++ layouts/Contact.js
@@ -40,7 +40,7 @@ const Contact = ({ data }) => {
               "h1 my-10 lg:my-11 lg:pt-11 text-center lg:text-left lg:text-[64px]"
             )}
           </div>
-          <div className="rounded border border-border p-6 dark:border-darkmode-border lg:col-6">
+          <div className="contact-form-wrapper rounded border border-border p-6 dark:border-darkmode-border lg:col-6">
             <h2>
               Send Us A
               <span className="ml-1.5 inline-flex items-center text-primary">
